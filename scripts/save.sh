0394fff01dd3fe862cef0b791c126df3ed742c0e
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-07 21:20:23 -0300

    1.0

--- scripts/save.sh
+++ scripts/save.sh
@@ -26,7 +26,9 @@ sed -i "s/\"version-name\": [0-9]\+/\"version-name\": $VERSION/" metadata.json
 # Reduce version to 2 digits
 VERSION=$(echo $VERSION | cut -d'.' -f1-2)
 # Update version in metadata.json
+echo "Updating version in metadata.json..."
 sed -i "s/\"version\": [0-9]\+/\"version\": $VERSION/" metadata.json
+
 
 # Add, commit and push files
 cd -- "$( dirname "$0" )/../"
