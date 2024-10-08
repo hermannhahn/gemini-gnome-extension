06d7c208c8c75cecd610782886987c6a579c9a41
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-07 22:34:02 -0300

    1.0.0

--- scripts/save.sh
+++ scripts/save.sh
@@ -22,13 +22,16 @@ fi
 
 # Update version-name in metadata.json
 echo "Updating version in metadata.json..."
-sed -i "s/\"version-\name\": [0-9]\+/\"version-\name\": $VERSION/" metadata.json
-# Reduce version to 2 digits
-VERSION=$(echo $VERSION | cut -d'.' -f1-2)
 # Update version in metadata.json
-echo "Updating version in metadata.json..."
-sed -i "s/\"version\": [0-9]\+\[0-9]\+/\"version\": $VERSION/" metadata.json
-
+# Reduce version to 2 digits
+major_version=$(echo $VERSION | cut -d. -f1)
+minor_version=$(echo $VERSION | cut -d. -f2)
+rversion="$major_version.$minor_version"
+jq ".version = \"$rversion\"" metadata.json > metadata.json.new
+mv metadata.json.new metadata.json
+sed -i "s/version-name:.*$/version-name: $VERSION/" metadata.json
+#jq ".version-name = \"$VERSION\"" metadata.json > metadata.json.new
+echo "Updated version in metadata.json"
 
 # Add, commit and push files
 cd -- "$( dirname "$0" )/../"
