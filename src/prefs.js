dd6039caa5b957c3e93ea79285850e72b6d2c3cd
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-08 17:33:57 -0300

    1.1

--- src/prefs.js
+++ src/prefs.js
@@ -31,6 +31,7 @@ class GeminiSettings {
             row_homogeneous: false,
         });
         const defaultKey = this.schema.get_string('gemini-api-key');
+        const defaultGKey = this.schema.get_string('google-api-key');
         const defaultFolder = this.schema.get_string('drive-folder');
         const defaultLog = this.schema.get_boolean('log-history');
         const defaultVertex = this.schema.get_boolean('vertex-enabled');
@@ -45,6 +46,18 @@ class GeminiSettings {
             buffer: new Gtk.EntryBuffer(),
         });
         const howToButton = new Gtk.LinkButton({
+            label: _('How to get API key?'),
+            uri: 'https://github.com/wwardaww/gnome-gemini-ai?tab=readme-ov-file#using-gemini-api-key',
+        });
+
+        const labelGoogle = new Gtk.Label({
+            label: _('Gemini API Key'),
+            halign: Gtk.Align.START,
+        });
+        const googleApiKey = new Gtk.Entry({
+            buffer: new Gtk.EntryBuffer(),
+        });
+        const howToButtonGoogle = new Gtk.LinkButton({
             label: _('How to get API key?'),
             uri: 'https://github.com/wwardaww/gnome-gemini-ai?tab=readme-ov-file#using-gemini-api-key',
         });
@@ -88,12 +101,17 @@ class GeminiSettings {
         histroyButton.set_active(defaultLog);
         VertexButton.set_active(defaultVertex);
         apiKey.set_text(defaultKey);
+        googleApiKey.set_text(defaultGKey);
         VertexProject.set_text(defaultVertexProject);
         folderUrl.set_text(defaultFolder);
         save.connect('clicked', () => {
             this.schema.set_string(
                 'gemini-api-key',
                 apiKey.get_buffer().get_text(),
+            );
+            this.schema.set_string(
+                'google-api-key',
+                googleApiKey.get_buffer().get_text(),
             );
             this.schema.set_string(
                 'drive-folder',
@@ -113,20 +131,24 @@ class GeminiSettings {
         this.main.attach(apiKey, 2, 0, 2, 1);
         this.main.attach(howToButton, 4, 0, 1, 1);
 
-        this.main.attach(labelFolder, 0, 1, 1, 1);
-        this.main.attach(folderUrl, 2, 1, 2, 1);
+        this.main.attach(labelGoogle, 0, 1, 1, 1);
+        this.main.attach(googleApiKey, 2, 1, 2, 1);
+        this.main.attach(howToButtonGoogle, 4, 1, 1, 1);
 
-        this.main.attach(histroyLabel, 0, 2, 1, 1);
-        this.main.attach(histroyButton, 2, 2, 1, 1);
+        this.main.attach(labelFolder, 0, 2, 1, 1);
+        this.main.attach(folderUrl, 2, 2, 2, 1);
 
-        this.main.attach(vertexLabel, 0, 3, 1, 1);
-        this.main.attach(VertexButton, 2, 3, 1, 1);
+        this.main.attach(histroyLabel, 0, 3, 1, 1);
+        this.main.attach(histroyButton, 2, 3, 1, 1);
 
-        this.main.attach(vertexProjectLabel, 0, 4, 1, 1);
-        this.main.attach(VertexProject, 2, 4, 2, 1);
+        this.main.attach(vertexLabel, 0, 4, 1, 1);
+        this.main.attach(VertexButton, 2, 4, 1, 1);
 
-        this.main.attach(save, 2, 5, 1, 1);
-        this.main.attach(statusLabel, 0, 6, 4, 1);
+        this.main.attach(vertexProjectLabel, 0, 5, 1, 1);
+        this.main.attach(VertexProject, 2, 5, 2, 1);
+
+        this.main.attach(save, 2, 6, 1, 1);
+        this.main.attach(statusLabel, 0, 7, 5, 1);
 
         this.ui.add(this.main);
     }
