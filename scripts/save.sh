a35b0248f12a7dceacc290b6696d5dd12499da65
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-11 16:26:48 -0300

    1.3

--- scripts/save.sh
+++ scripts/save.sh
@@ -8,21 +8,35 @@
 # Main script starts here #
 ###########################
 
-
 # Get version from package.json
-VERSION=$(jq -r '.version' package.json)
+VERSION=$(jq -r .version package.json) # E.g. 1.0.0
 
-echo "Current Version: $VERSION"
+# Get branch name
+BRANCH=$(git rev-parse --abbrev-ref HEAD) # E.g. main or 1.0.0
 
-# Check if comment is empty
+
+# Check if version is empty
 if [ -z "$VERSION" ]; then
   echo "Please type a version in package.json for the commit."
   exit 1
 fi
+# Check if is main branch
+if [ "$BRANCH" == "main" ]; then
+  echo "Please change to a version branch."
+  exit 1
+fi
+
+# Compare VERSION with branch name.
+if [ "$BRANCH" != "$VERSION" ]; then
+  # Update version in package.json with branch name
+  jq ".version = \"$BRANCH\"" package.json > package.json.new
+  mv package.json.new package.json
+  echo "Updated version in package.json"
+fi
 
 # Update version-name in metadata.json
-minor_version=$(echo "$VERSION" | cut -d'.' -f2)
-major_version=($(echo "$VERSION" | cut -d'.' -f1))
+minor_version=$(echo "$BRANCH" | cut -d'.' -f2)
+major_version=($(echo "$BRANCH" | cut -d'.' -f1))
 if [ "$minor_version" -eq 9 ]; then
   minor_version=0
   major_version=$((major_version + 1))
@@ -31,6 +45,8 @@ else
 fi
 
 VERSION=$major_version.$minor_version
+
+echo "Current Version: $VERSION"
 
 # Update
 jq ".version = $VERSION" metadata.json > metadata.json.new
@@ -43,7 +59,7 @@ npm run build:install
 # Add, commit and push files
 cd -- "$( dirname "$0" )/../"
 git add .
-git commit -m "$VERSION"
+git commit -S -m "$VERSION"
 git push
 
 echo "All done."
