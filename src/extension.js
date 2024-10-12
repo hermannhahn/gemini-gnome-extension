3ae963851f8600084bc9ed9c1487cb458463b68b
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 03:13:27 -0300

    1.2.4

--- src/extension.js
+++ src/extension.js
@@ -280,7 +280,7 @@ export default class GeminiExtension extends Extension {
             openSettings: this.openPreferences,
             uuid: this.uuid,
         });
-        Main.panel.addToStatusArea('geminiAiUbuntu', this._gemini, 1);
+        Main.panel.addToStatusArea('gvaGnomeExtension', this._gemini, 1);
         _httpSession.send_and_read_async(
             message,
             GLib.PRIORITY_DEFAULT,
