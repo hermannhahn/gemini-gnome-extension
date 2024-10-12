441016d12a6bffd2fed03ea385c717f3dcb0605e
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 17:11:26 -0300

    1.2.4

--- src/text_format.js
+++ src/text_format.js
@@ -6,15 +6,12 @@
 export function format(text) {
     // Replace all * char with space
     text = text.split('*').join(' ');
-    // Replace all ` char with space
-    text = text.split('`').join(' ');
-    // Replace all \n char with <br>
-    text = text.split('\n').join('<br>');
-    // Replace all & char with &amp;
-    text = text.split('&').join('&amp;');
-    // Replace all < char with &lt;
-    text = text.split('<').join('&lt;');
-    // Replace all > char with &gt;
-    text = text.split('>').join('&gt;');
+    // Break line if greater then 900px
+    text = text.replace(/(.{900})/g, '$1\n');
+    // Replace all ` with <code>
+    text = text.replace(/`/g, '<code>');
+    // Replace all </code> with `
+    text = text.replace(/<\/code>/g, '`');
+
     return text;
 }
