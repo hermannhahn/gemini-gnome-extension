3cf7306527d5f4f70f97acdd192e01ad988026e5
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 00:12:02 -0300

    1.2.4

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,24 @@
+#!/bin/bash -e
+
+# ==============================================================================
+# This script install dependencies and the extension.
+# ==============================================================================
+
+echo "Downloading extension..."
+git clone https://github.com/hermannhahn/gemini-gnome-extension.git
+cd gemini-gnome-extension
+echo "Extension cloned."
+echo "Installing dependencies..."
 sudo apt update
 sudo apt install curl -y
 sudo apt install python3 -y
 sudo apt install notify-send -y
 sudo apt install sox -y
-git clone https://github.com/hermannhahn/gemini-gnome-extension.git
-cd gemini-gnome-extension
 npm install
+echo "Dependencies installed."
+echo "Installing extension..."
 npm run build:install
-
+echo "Extension installed."
+echo "All done."
+echo "Active Gemini Voice Assistant: https://extensions.gnome.org/local/"
+exit 0
