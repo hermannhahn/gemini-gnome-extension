b93a3cfafe39b02fee1c64f90bc6d2718d50fcc3
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 03:02:16 -0300

    1.2.4

---
+++ src/auth.js
@@ -0,0 +1,12 @@
+import GLib from 'gi://GLib';
+
+/**
+ *
+ */
+export function generateAPIKey() {
+    let keyStream = GLib.spawn_command_line_sync(
+        'gcloud auth application-default print-access-token',
+    ).toString();
+    const keyArray = keyStream.split(',');
+    return keyArray[1].replace(/(\r\n|\n|\r)/gm, '');
+}
