3780562a772903b0925827f9007ad3d36063d521
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 17:03:00 -0300

    1.2.4

--- src/text_format.js
+++ src/text_format.js
@@ -4,5 +4,17 @@
  * @param {string} text
  */
 export function format(text) {
+    // Replace all * char with space
+    text = text.split('*').join(' ');
+    // Replace all ` char with space
+    text = text.split('`').join(' ');
+    // Replace all \n char with <br>
+    text = text.split('\n').join('<br>');
+    // Replace all & char with &amp;
+    text = text.split('&').join('&amp;');
+    // Replace all < char with &lt;
+    text = text.split('<').join('&lt;');
+    // Replace all > char with &gt;
+    text = text.split('>').join('&gt;');
     return text;
 }
