6d706034f75d5e3a88d92da5589af3544e888e95
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 00:05:39 -0300

    1.2.4

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,8 @@ sudo apt install curl -y
 sudo apt install python3 -y
 sudo apt install notify-send -y
 sudo apt install sox -y
+git clone https://github.com/hermannhahn/gemini-gnome-extension.git
+cd gemini-gnome-extension
 npm install
+npm run build:install
+
