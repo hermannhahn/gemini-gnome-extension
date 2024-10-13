63d493f669b80c2aea80de67cec94d3fb1e4f62c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-12 23:40:47 -0300

    1.2.4

--- src/text_format.js
+++ src/text_format.js
@@ -8,10 +8,5 @@ export function format(text) {
     text = text.split('*').join(' ');
     // Break line if greater then 900px
     text = text.replace(/(.{900})/g, '$1\n');
-    // Replace all ` with <code>
-    text = text.replace(/`/g, '<code>');
-    // Replace all </code> with `
-    text = text.replace(/<\/code>/g, '`');
-
     return text;
 }
