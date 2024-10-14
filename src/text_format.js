c977502f6ad916525114de0bd4bdb1be5d713858
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-14 20:17:25 -0300

    1.2.5

--- src/text_format.js
+++ src/text_format.js
@@ -3,33 +3,43 @@
 /**
  * @param {string} text
  */
-export function format(text) {
+export function format(text, breakLines = false) {
     let formatedText = text
-        .replace(/\*/g, ' ') // Remove all * char from text
-        .replace(/`{3}\s*\w+\s*/g, '`{3}') // Remove first word after triple ` char
-        .replace(/`{3}/g, ''); // Replace triple ` char
+        .replace(/<\/?p>/g, '\n') // <p> -> quebra de linha
+        .replace(/<\/?b>/g, '<b>') // <b> já é suportado
+        .replace(/<\/?i>/g, '<i>') // <i> já é suportado
+        .replace(/<\/?u>/g, '<u>') // <u> já é suportado
+        .replace(/<li>/g, '• ') // <li> -> bullet point
+        .replace(/<\/li>/g, '\n') // Fecha <li> -> nova linha
+        .replace(/<br\s*\/?>/g, '\n') // <br> -> nova linha
+        .replace(/<\/?a[^>]*>/g, '') // Remove as tags <a>
+        .replace(/<\/a>/g, '') // Remove tags de fechamento <a>
+        .replace(/<a href="([^"]+)">/g, '<u>$1</u>'); // Converte links para texto sublinhado;
 
     // Line breaker
-    let lines = formatedText.split('\n');
-    let result = '';
-    for (let i = 0; i < lines.length; i++) {
-        let line = lines[i];
-        if (line.length > 90) {
-            let words = line.split(' ');
-            let currentLine = '';
-            for (let j = 0; j < words.length; j++) {
-                let word = words[j];
-                if (currentLine.length + word.length > 90) {
-                    result += currentLine + '\n';
-                    currentLine = word + ' ';
-                } else {
-                    currentLine += word + ' ';
+    if (breakLines) {
+        let lines = formatedText.split('\n');
+        let result = '';
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+            if (line.length > 90) {
+                let words = line.split(' ');
+                let currentLine = '';
+                for (let j = 0; j < words.length; j++) {
+                    let word = words[j];
+                    if (currentLine.length + word.length > 90) {
+                        result += currentLine + '\n';
+                        currentLine = word + ' ';
+                    } else {
+                        currentLine += word + ' ';
+                    }
                 }
+                result += currentLine + '\n';
+            } else {
+                result += line + '\n';
             }
-            result += currentLine + '\n';
-        } else {
-            result += line + '\n';
         }
+        return result;
     }
-    return result;
+    return formatedText;
 }
