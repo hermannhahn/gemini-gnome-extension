138ed3557c3d12009a7846ba855dd4cac9ee2267
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-14 16:33:28 -0300

    1.2.5

--- src/text_format.js
+++ src/text_format.js
@@ -4,14 +4,23 @@
  * @param {string} text
  */
 export function format(text) {
-    // Remove all * char from text
-    text = text.replace(/\*/g, ' ');
-    // Remove first word after triple ` char
-    text = text.replace(/`{3}\s*\w+\s*/g, '`{3}');
-    // Replace triple ` char
-    text = text.replace(/`{3}/g, '');
+    let html = text
+        .replace(/<\/?p>/g, '\n') // <p> -> quebra de linha
+        .replace(/<\/?b>/g, '<b>') // <b> já é suportado
+        .replace(/<\/?i>/g, '<i>') // <i> já é suportado
+        .replace(/<\/?u>/g, '<u>') // <u> já é suportado
+        .replace(/<li>/g, '• ') // <li> -> bullet point
+        .replace(/<\/li>/g, '\n') // Fecha <li> -> nova linha
+        .replace(/<ul>|<\/ul>/g, '') // Remove <ul> tags
+        .replace(/<ol>/g, '') // Remove <ol> tag
+        .replace(/<\/ol>/g, '') // Remove </ol> tag
+        .replace(/<br\s*\/?>/g, '\n') // <br> -> nova linha
+        .replace(/\*/g, ' ') // Remove all * char from text
+        .replace(/`{3}\s*\w+\s*/g, '`{3}') // Remove first word after triple ` char
+        .replace(/`{3}/g, ''); // Replace triple ` char
+
     // Line breaker
-    let lines = text.split('\n');
+    let lines = html.split('\n');
     let result = '';
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
