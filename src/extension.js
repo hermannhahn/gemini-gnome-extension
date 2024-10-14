f70ca3cc715f77a3f4ef15032fac115b469e315a
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-13 23:35:26 -0300

    1.2.5

--- src/extension.js
+++ src/extension.js
@@ -374,33 +374,6 @@ const Gemini = GObject.registerClass(
             super.destroy();
         }
 
-        // Line breaker
-        lineBreaker(text) {
-            // Break lines each 90 characters, but dont break the word.
-            let lines = text.split('\n');
-            let result = '';
-            for (let i = 0; i < lines.length; i++) {
-                let line = lines[i];
-                if (line.length > 90) {
-                    let words = line.split(' ');
-                    let currentLine = '';
-                    for (let j = 0; j < words.length; j++) {
-                        let word = words[j];
-                        if (currentLine.length + word.length > 90) {
-                            result += currentLine + '\n';
-                            currentLine = word + ' ';
-                        } else {
-                            currentLine += word + ' ';
-                        }
-                    }
-                    result += currentLine + '\n';
-                } else {
-                    result += line + '\n';
-                }
-            }
-            return result;
-        }
-
         // Create history.json file if not exist
         createHistoryFile() {
             if (!GLib.file_test(historyFilePath, GLib.FileTest.IS_REGULAR)) {
