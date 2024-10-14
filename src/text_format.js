3fbe9e334097fbd527b039c4414aa51e8c7773bc
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-13 23:35:16 -0300

    1.2.5

--- src/text_format.js
+++ src/text_format.js
@@ -4,9 +4,26 @@
  * @param {string} text
  */
 export function format(text) {
-    // Replace all * char with space
-    text = text.split('*').join(' ');
-    // Break line if greater then 900px
-    text = text.replace(/(.{900})/g, '$1\n');
-    return text;
+    let lines = text.split('\n');
+    let result = '';
+    for (let i = 0; i < lines.length; i++) {
+        let line = lines[i];
+        if (line.length > 90) {
+            let words = line.split(' ');
+            let currentLine = '';
+            for (let j = 0; j < words.length; j++) {
+                let word = words[j];
+                if (currentLine.length + word.length > 90) {
+                    result += currentLine + '\n';
+                    currentLine = word + ' ';
+                } else {
+                    currentLine += word + ' ';
+                }
+            }
+            result += currentLine + '\n';
+        } else {
+            result += line + '\n';
+        }
+    }
+    return result;
 }
