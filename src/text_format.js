be77553793c1d3659f06ed774ccd0a550d0b3fee
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-14 21:05:09 -0300

    1.2.5

--- src/text_format.js
+++ src/text_format.js
@@ -1,12 +1,7 @@
 // Text Format
 
-/**
- * @param {string} text - Text to be formatted
- * @param {boolean} breakLines - Break lines if text is too long
- * @returns {string} Formatted text
- */
 export class Formatter {
-    format(text, breakLines = false) {
+    pango(text) {
         let formatedText = text
             .replace(/<\/?p>/g, '\n') // <p> -> quebra de linha
             .replace(/<li>/g, '• ') // <li> -> bullet point
@@ -16,31 +11,32 @@ export class Formatter {
             .replace(/<\/a>/g, '') // Remove tags de fechamento <a>
             .replace(/<a href="([^"]+)">/g, '<u>$1</u>'); // Converte links para texto sublinhado;
 
-        // Line breaker
-        if (breakLines) {
-            let lines = formatedText.split('\n');
-            let result = '';
-            for (let i = 0; i < lines.length; i++) {
-                let line = lines[i];
-                if (line.length > 90) {
-                    let words = line.split(' ');
-                    let currentLine = '';
-                    for (let j = 0; j < words.length; j++) {
-                        let word = words[j];
-                        if (currentLine.length + word.length > 90) {
-                            result += currentLine + '\n';
-                            currentLine = word + ' ';
-                        } else {
-                            currentLine += word + ' ';
-                        }
-                    }
-                    result += currentLine + '\n';
-                } else {
-                    result += line + '\n';
-                }
-            }
-            return result;
-        }
         return formatedText;
+    }
+
+    // Line breaker
+    breakLines(text) {
+        let lines = text.split('\n');
+        let result = '';
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+            if (line.length > 90) {
+                let words = line.split(' ');
+                let currentLine = '';
+                for (let j = 0; j < words.length; j++) {
+                    let word = words[j];
+                    if (currentLine.length + word.length > 90) {
+                        result += currentLine + '\n';
+                        currentLine = word + ' ';
+                    } else {
+                        currentLine += word + ' ';
+                    }
+                }
+                result += currentLine + '\n';
+            } else {
+                result += line + '\n';
+            }
+        }
+        return result;
     }
 }
