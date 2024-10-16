5944e81eaea0c7f1ad106ff9a17f238650e96440
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-16 12:34:19 -0300

    1.2.5

--- src/extension.js
+++ src/extension.js
@@ -218,11 +218,9 @@ const Gemini = GObject.registerClass(
             // Create input and response chat items
             const inputChat = new PopupMenu.PopupMenuItem('', {
                 style_class: 'input-chat',
-                hover: true,
             });
             const responseChat = new PopupMenu.PopupMenuItem('', {
                 style_class: 'response-chat',
-                hover: true,
             });
 
             // Add user question to chat
