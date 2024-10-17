666377596601de76fb12be023e2091e68914657c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-16 22:57:23 -0300

    1.2.5

--- src/extension.js
+++ src/extension.js
@@ -238,8 +238,8 @@ const Gemini = GObject.registerClass(
             });
 
             // Add user question to chat
-            let formatedQuestion = convertMD(formatedQuestion);
-            formatedQuestion = format.inputChat(userQuestion);
+            let formatedQuestion = convertMD(userQuestion);
+            formatedQuestion = format.inputChat(formatedQuestion);
             inputChat.label.clutter_text.set_markup(
                 `<b>${USERNAME}: </b>${formatedQuestion}`,
             );
