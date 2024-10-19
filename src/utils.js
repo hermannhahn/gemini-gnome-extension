179d8fb1a2102dbddc27f394c47ade54e030954c
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-19 16:03:43 -0300

    1.3.0

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,14 @@ export default class Utils {
     constructor() {
         console.log('Utils loaded');
     }
+
+    /**
+     *
+     * @param {*} message
+     */
+    log(message) {
+        if (message) {
+            console.log(`[ DEBUG ] ${message}`);
+        }
+    }
 }
