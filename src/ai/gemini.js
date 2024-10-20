72affc9829c0722c5b7c112e90faed8f687c7fec
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-20 00:25:30 -0300

    1.3.0

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -24,7 +24,6 @@ export default class GoogleGemini {
         AZURE_SPEECH_LANGUAGE,
         AZURE_SPEECH_VOICE,
     ) {
-        console.log('Gemini Voice Assistant loaded');
         this.USERNAME = GLib.get_real_name();
         this.LOCATION = '';
         this.GEMINIAPIKEY = GEMINIAPIKEY;
@@ -32,6 +31,7 @@ export default class GoogleGemini {
         this.AZURE_SPEECH_REGION = AZURE_SPEECH_REGION;
         this.AZURE_SPEECH_LANGUAGE = AZURE_SPEECH_LANGUAGE;
         this.AZURE_SPEECH_VOICE = AZURE_SPEECH_VOICE;
+        console.log('Gemini Voice Assistant loaded');
     }
 
     /**
