4feb4fd9bb67cf0ece2df17f6129b180acf0f642
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-20 23:11:19 -0300

    1.3.0

--- src/extension.js
+++ src/extension.js
@@ -263,7 +263,7 @@ const Aiva = GObject.registerClass(
             // Add user question to chat
             userQuestion = utils.inputformat(userQuestion);
             inputChat.label.clutter_text.set_markup(
-                `<b>Me: </b>${userQuestion}`,
+                `<b>${_('Mes')}: </b>${userQuestion}`,
             );
 
             // Get ai response for user question
