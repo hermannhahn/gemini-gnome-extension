9b35324187a7b102ed5ebba700fe83ad58cef03d
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-22 15:08:58 -0300

    1.3.0

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -24,92 +24,89 @@ export class GoogleGemini {
      * @description Return ai response
      */
     response(question) {
-        new Promise((resolve, reject) => {
-            // Create http session
-            let _httpSession = new Soup.Session();
-            let url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${this.GEMINIAPIKEY}`;
-            let aiResponse = null;
+        // Create http session
+        let _httpSession = new Soup.Session();
+        let url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${this.GEMINIAPIKEY}`;
+        let aiResponse = null;
 
-            // Compose request
-            var body = this._buildBody(question);
-            let message = Soup.Message.new('POST', url);
-            let bytes = GLib.Bytes.new(body);
-            message.set_request_body_from_bytes('application/json', bytes);
+        // Compose request
+        var body = this._buildBody(question);
+        let message = Soup.Message.new('POST', url);
+        let bytes = GLib.Bytes.new(body);
+        message.set_request_body_from_bytes('application/json', bytes);
 
-            // Send async request
-            _httpSession.send_and_read_async(
-                message,
-                GLib.PRIORITY_DEFAULT,
-                null,
-                (_httpSession, result) => {
-                    let bytes = _httpSession.send_and_read_finish(result);
-                    let decoder = new TextDecoder('utf-8');
+        // Send async request
+        _httpSession.send_and_read_async(
+            message,
+            GLib.PRIORITY_DEFAULT,
+            null,
+            (_httpSession, result) => {
+                let bytes = _httpSession.send_and_read_finish(result);
+                let decoder = new TextDecoder('utf-8');
 
-                    // Get response
-                    let response = decoder.decode(bytes.get_data());
-                    let res = JSON.parse(response);
-                    if (res.error?.code !== 401 && res.error !== undefined) {
-                        logError(res.error);
-                        aiResponse = 'Sorry, error getting response.';
-                    }
-                    aiResponse = res.candidates[0]?.content?.parts[0]?.text;
+                // Get response
+                let response = decoder.decode(bytes.get_data());
+                let res = JSON.parse(response);
+                if (res.error?.code !== 401 && res.error !== undefined) {
+                    logError(res.error);
+                    aiResponse = 'Sorry, error getting response.';
+                }
+                aiResponse = res.candidates[0]?.content?.parts[0]?.text;
 
-                    // SAFETY warning
-                    if (res.candidates[0].finishReason === 'SAFETY') {
-                        for (
-                            let i = 0;
-                            i < res.candidates[0].safetyRatings.length;
-                            i++
-                        ) {
-                            let safetyRating =
-                                res.candidates[0].safetyRatings[i];
-                            if (safetyRating.probability !== 'NEGLIGIBLE') {
-                                if (
-                                    safetyRating.category ===
-                                    'HARM_CATEGORY_SEXUALLY_EXPLICIT'
-                                ) {
-                                    aiResponse = _(
-                                        "Sorry, I can't answer this question. Possible sexually explicit content in the question or answer.",
-                                    );
-                                }
-                                if (
-                                    safetyRating.category ===
-                                    'HARM_CATEGORY_HATE_SPEECH'
-                                ) {
-                                    aiResponse = _(
-                                        "Sorry, I can't answer this question. Possible hate speech in the question or answer.",
-                                    );
-                                }
-                                if (
-                                    safetyRating.category ===
-                                    'HARM_CATEGORY_HARASSMENT'
-                                ) {
-                                    aiResponse = _(
-                                        "Sorry, I can't answer this question. Possible harassment in the question or answer.",
-                                    );
-                                }
-                                if (
-                                    safetyRating.category ===
-                                    'HARM_CATEGORY_DANGEROUS_CONTENT'
-                                ) {
-                                    aiResponse = _(
-                                        "Sorry, I can't answer this question. Possible dangerous content in the question or answer.",
-                                    );
-                                }
+                // SAFETY warning
+                if (res.candidates[0].finishReason === 'SAFETY') {
+                    for (
+                        let i = 0;
+                        i < res.candidates[0].safetyRatings.length;
+                        i++
+                    ) {
+                        let safetyRating = res.candidates[0].safetyRatings[i];
+                        if (safetyRating.probability !== 'NEGLIGIBLE') {
+                            if (
+                                safetyRating.category ===
+                                'HARM_CATEGORY_SEXUALLY_EXPLICIT'
+                            ) {
+                                aiResponse = _(
+                                    "Sorry, I can't answer this question. Possible sexually explicit content in the question or answer.",
+                                );
+                            }
+                            if (
+                                safetyRating.category ===
+                                'HARM_CATEGORY_HATE_SPEECH'
+                            ) {
+                                aiResponse = _(
+                                    "Sorry, I can't answer this question. Possible hate speech in the question or answer.",
+                                );
+                            }
+                            if (
+                                safetyRating.category ===
+                                'HARM_CATEGORY_HARASSMENT'
+                            ) {
+                                aiResponse = _(
+                                    "Sorry, I can't answer this question. Possible harassment in the question or answer.",
+                                );
+                            }
+                            if (
+                                safetyRating.category ===
+                                'HARM_CATEGORY_DANGEROUS_CONTENT'
+                            ) {
+                                aiResponse = _(
+                                    "Sorry, I can't answer this question. Possible dangerous content in the question or answer.",
+                                );
                             }
                         }
                     }
-                },
-                (error, debug) => {
-                    logError(error);
-                    log(debug);
-                    aiResponse = 'Sorry, error getting response.';
-                    reject(error);
-                },
-            );
-            aiResponse = utils.textformat(aiResponse);
-            resolve(aiResponse);
-        });
+                }
+            },
+            (error, debug) => {
+                logError(error);
+                log(debug);
+                aiResponse = 'Sorry, error getting response.';
+                return error;
+            },
+        );
+        aiResponse = utils.textformat(aiResponse);
+        return aiResponse;
     }
 
     /**
