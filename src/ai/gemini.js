f6ecbe0cc2934ed03829b10d9788c888a388c281
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-22 23:05:11 -0300

    1.3.0

--- src/ai/gemini.js
+++ src/ai/gemini.js
@@ -1,6 +1,7 @@
 import Soup from 'gi://Soup';
 import GLib from 'gi://GLib';
 import {Utils} from '../utils/utils.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import {gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
@@ -14,6 +15,86 @@ export class GoogleGemini {
     constructor(GEMINIAPIKEY) {
         this.GEMINIAPIKEY = GEMINIAPIKEY;
         console.log('Gemini Voice Assistant loaded');
+    }
+
+    /**
+     *
+     * @param {*} userQuestion
+     */
+    chat(userQuestion) {
+        // Question
+        const inputChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'input-chat',
+            reactive: true,
+            can_focus: false,
+            hover: true,
+        });
+        inputChat.label.clutter_text.reactive = true;
+        inputChat.label.clutter_text.selectable = true;
+        inputChat.label.clutter_text.hover = false;
+        inputChat.label.x_expand = true;
+
+        // Response
+        const responseChat = new PopupMenu.PopupMenuItem('', {
+            style_class: 'response-chat',
+            reactive: true,
+            can_focus: false,
+            hover: true,
+        });
+        responseChat.label.clutter_text.reactive = true;
+        responseChat.label.clutter_text.selectable = true;
+        responseChat.label.clutter_text.hover = false;
+        responseChat.label.x_expand = true;
+
+        // Copy button
+        const copyButton = new PopupMenu.PopupMenuItem('', {
+            style_class: 'copy-icon',
+            reactive: true,
+            can_focus: false,
+            hover: false,
+        });
+        copyButton.connect('activate', (_self) => {
+            utils.copySelectedText(responseChat, copyButton, this.extension);
+        });
+
+        // Separator
+        const newSeparator = new PopupMenu.PopupSeparatorMenuItem();
+
+        // Add user question and ai response to chat
+        this.chatSection.addMenuItem(newSeparator);
+        this.chatSection.addMenuItem(inputChat);
+        this.chatSection.addMenuItem(responseChat);
+
+        // Add user question to chat
+        userQuestion = utils.inputformat(userQuestion);
+        log('Question: ' + userQuestion);
+        inputChat.label.clutter_text.set_markup(
+            `<b>${_('Me')}: </b>${userQuestion}`,
+        );
+
+        // Add ai temporary response to chat
+        responseChat.label.clutter_text.set_markup('<b>Gemini: </b> ...');
+
+        // Build question
+        let question = {
+            userQuestion,
+            responseChat,
+            searchEntry: this.searchEntry,
+            chatHistory: this.chatHistory,
+            recursiveTalk: this.config.RECURSIVETALK,
+        };
+
+        // Get ai response for user question
+        this.gemini.response(question);
+
+        // Scroll down
+        utils.scrollToBottom(responseChat, this.scrollView);
+
+        // Add copy button to chat
+        this.chatSection.addMenuItem(copyButton);
+
+        // Scroll down
+        utils.scrollToBottom(responseChat, this.scrollView);
     }
 
     /**
