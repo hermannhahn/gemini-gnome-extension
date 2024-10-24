ee90227f7e715fc47a77962f12d62048bc43901b
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-24 18:29:34 -0300

    1.3.0

--- src/extension.js
+++ src/extension.js
@@ -96,6 +96,7 @@ const Aiva = GObject.registerClass(
         _init(extension) {
             this.keyLoopBind = 0;
             this.extension = extension;
+            this.userSettings = {};
             super._init(0.0, _('Gemini Voice Assistant for Ubuntu'));
             this._loadSettings();
             this.recursiveHistory = [];
