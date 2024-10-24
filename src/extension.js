325fc9fb4fd038b8ccdba0455058457cc7521f67
Hermann Hahn <hermann.h.hahn@gmail.com>
2024-10-24 12:14:02 -0300

    1.3.0

--- src/extension.js
+++ src/extension.js
@@ -13,6 +13,7 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import {Utils} from './utils/utils.js';
+import {AppLayout} from './ui.js';
 
 // Global variables
 let pipeline;
@@ -72,92 +73,7 @@ const Gemini = GObject.registerClass(
             this.settings.USERNAME = GLib.get_real_name();
             this.settings.LOCATION = '';
             this.chatHistory = [];
-            this._ui();
-        }
-
-        _ui() {
-            this.tray = new St.BoxLayout({
-                style_class: 'panel-status-menu-box',
-            });
-            this.icon = new St.Icon({
-                style_class: 'google-gemini-icon',
-            });
-            // Create app item section
-            this.item = new PopupMenu.PopupBaseMenuItem({
-                reactive: false,
-                can_focus: false,
-            });
-
-            // Create search entry
-            this.searchEntry = new St.Entry({
-                name: 'aiEntry',
-                style_class: 'ai-entry',
-                can_focus: true,
-                hint_text: _('Ask me anything...'),
-                track_hover: true,
-                x_expand: true,
-                y_expand: true,
-            });
-
-            // Create voice activation button
-            this.micButton = new St.Button({
-                can_focus: true,
-                toggle_mode: true,
-                style_class: 'mic-icon',
-            });
-
-            // Create clear history button
-            this.clearButton = new St.Button({
-                can_focus: true,
-                toggle_mode: true,
-                style_class: 'trash-icon',
-            });
-
-            // Create settings button
-            this.settingsButton = new St.Button({
-                can_focus: true,
-                toggle_mode: true,
-                style_class: 'settings-icon',
-            });
-
-            // Create chat section
-            this.chatSection = new PopupMenu.PopupMenuSection({
-                style_class: 'chat-section',
-                x_expand: true,
-                y_expand: true,
-            });
-
-            // Create scrollbar
-            this.scrollView = new St.ScrollView({
-                style_class: 'chat-scroll-section',
-                reactive: true,
-                overlay_scrollbars: false,
-            });
-
-            // Create input and response chat items
-            this.inputChat = new PopupMenu.PopupMenuItem('', {
-                style_class: 'input-chat',
-                reactive: true,
-                can_focus: false,
-                hover: true,
-            });
-            this.responseChat = new PopupMenu.PopupMenuItem('', {
-                style_class: 'response-chat',
-                reactive: true,
-                can_focus: false,
-                hover: true,
-            });
-
-            // Create copy button
-            this.copyButton = new PopupMenu.PopupMenuItem('', {
-                style_class: 'copy-icon',
-                reactive: true,
-                can_focus: false,
-                hover: false,
-            });
-
-            // Separator
-            this.newSeparator = new PopupMenu.PopupSeparatorMenuItem();
+            this.ui = new AppLayout();
         }
 
         _init(extension) {
